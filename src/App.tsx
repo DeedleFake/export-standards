2869c79db19c8ba1659eb260bcca60dfe0c932d4
DeedleFake <deedlefake@users.noreply.github.com>
2025-12-04 15:02:39 -0500

    run Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import type { ChangeEvent } from "react";
 import "./index.css";
 
 export function App() {
-	const updateLink = async (ev: ChangeEvent<HTMLInputElement>) => {
-		console.dir(ev)
-	}
+  const updateLink = async (ev: ChangeEvent<HTMLInputElement>) => {
+    console.dir(ev);
+  };
 
   return (
     <div className="flex flex-col items-center gap-2 p-8">
-			<input type="file" onChange={updateLink} />
+      <input type="file" onChange={updateLink} />
     </div>
   );
 }
