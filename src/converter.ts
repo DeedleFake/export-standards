ac2e427f680a04dddc7e95f520600fd83f0f73e7
DeedleFake <deedlefake@users.noreply.github.com>
2025-12-04 16:57:20 -0500

    add headers to output CSV

--- src/converter.ts
+++ src/converter.ts
@@ -2,15 +2,26 @@ import * as CSV from "csv-stringify/browser/esm/sync";
 
 export function convertToCSV(html: string): string {
   const tree = new DOMParser().parseFromString(html, "text/html");
+
+  const headers = Array.from(
+    tree.querySelectorAll<HTMLTableCellElement>(
+			"table#resultTable > thead > tr > th",
+    ),
+  ).map((data) => data.innerText);
+  if (headers.length == 0) {
+    throw new Error("count not find table headers");
+  }
+
   const rows = Array.from(
     tree.querySelectorAll<HTMLTableRowElement>(
       "table#resultTable > tbody > tr",
     ),
-  );
+  ).map(convertRow);
   if (rows.length === 0) {
     throw new Error("could not find table rows");
   }
-  return CSV.stringify(rows.map(convertRow));
+
+  return CSV.stringify([headers, ...rows]);
 }
 
 function convertRow(row: HTMLTableRowElement): Array<string> {
